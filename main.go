dab4e618251a4e15096efc91710b2535b324e981
mark.khomutckii <mark.khomutckii@devim.team>
2021-05-09 14:16:15 +0300

    refactor

--- main.go
+++ main.go
@@ -20,67 +20,74 @@ const (
 var (
 	samples = []*complexSample{
 		{
-			x_lower: -2.5,
-			x_upper: 1.0555555,
-			y_lower: -1,
-			y_upper: 1,
-			fname:   "image1.png",
+			real_lower: -2.5,
+			real_upper: 1.0555555,
+			imag_lower: -1,
+			imag_upper: 1,
+			fname:      "image1.png",
 		},
 		{
-			x_lower: -0.7512082704194535,
-			x_upper: -0.7189233668101299,
-			y_lower: 0.171229302623112,
-			y_upper: 0.1954429803301047,
-			fname:   "image2.png",
+			real_lower: -0.7512082704194535,
+			real_upper: -0.7189233668101299,
+			imag_lower: 0.171229302623112,
+			imag_upper: 0.1954429803301047,
+			fname:      "image2.png",
 		},
 		{
-			x_lower: -0.8430837175066829,
-			x_upper: -0.7097679463836053,
-			y_lower: 0.1298062156920607,
-			y_upper: 0.2297930440343688,
-			fname:   "image3.png",
+			real_lower: -0.8430837175066829,
+			real_upper: -0.7097679463836053,
+			imag_lower: 0.1298062156920607,
+			imag_upper: 0.2297930440343688,
+			fname:      "image3.png",
 		},
 		{
-			x_lower: -0.7436848654999562,
-			x_upper: -0.7382828140917086,
-			y_lower: 0.1820790907660431,
-			y_upper: 0.1861306293222289,
-			fname:   "image4.png",
+			real_lower: -0.7436848654999562,
+			real_upper: -0.7382828140917086,
+			imag_lower: 0.1820790907660431,
+			imag_upper: 0.1861306293222289,
+			fname:      "image4.png",
 		},
 		{
-			x_lower: -1.2578645782749025,
-			x_upper: -1.2575820364511403,
-			y_lower: 0.0293170055498651,
-			y_upper: 0.0295289119176868,
-			fname:   "image5.png",
+			real_lower: -1.2578645782749025,
+			real_upper: -1.2575820364511403,
+			imag_lower: 0.0293170055498651,
+			imag_upper: 0.0295289119176868,
+			fname:      "image5.png",
 		},
 		{
-			x_lower: -1.1590295228484262,
-			x_upper: -1.1590294146037756,
-			y_lower: 0.2725089787759562,
-			y_upper: 0.272509059959444,
-			fname:   "image6.png",
+			real_lower: -1.1590295228484262,
+			real_upper: -1.1590294146037756,
+			imag_lower: 0.2725089787759562,
+			imag_upper: 0.272509059959444,
+			fname:      "image6.png",
 		},
 		{
-			x_lower: 0.2634939406642726,
-			x_upper: 0.2645498803213035,
-			y_lower: 0.0025203902052182,
-			y_upper: 0.0033123449479914,
-			fname:   "image7.png",
+			real_lower: 0.2634939406642726,
+			real_upper: 0.2645498803213035,
+			imag_lower: 0.0025203902052182,
+			imag_upper: 0.0033123449479914,
+			fname:      "image7.png",
+		},
+		{
+			real_lower: -1.378023963943141,
+			real_upper: -1.376689723280189,
+			imag_lower: 0.0137389511214535,
+			imag_upper: 0.0147396316186676,
+			fname:      "image8.png",
 		},
 	}
 )
 
 type complexSample struct {
 	// min real value
-	x_lower float64
+	real_lower float64
 	// max real value
-	x_upper float64
+	real_upper float64
 
 	// min imaginary value
-	y_lower float64
+	imag_lower float64
 	// max imaginary value
-	y_upper float64
+	imag_upper float64
 
 	// image name to save on disk
 	fname string
@@ -136,9 +143,9 @@ func render(width, height int, cs *complexSample, p *palette.Gradient) *image.NR
 }
 
 func calcRgb(x, y, maxX, maxY int, cs *complexSample) (r, g, b uint8) {
-	cx := convertRange(float64(x), 0, float64(maxX), cs.x_lower, cs.x_upper)
-	cy := convertRange(float64(y), 0, float64(maxY), cs.y_lower, cs.y_upper)
-	iters := iter(cx, cy, max_iter)
+	cx := convertRange(float64(x), 0, float64(maxX), cs.real_lower, cs.real_upper)
+	cy := convertRange(float64(y), 0, float64(maxY), cs.imag_lower, cs.imag_upper)
+	iters := mandelbrotSet(cx, cy, max_iter)
 
 	color := uint8(convertRange(float64(iters), 0, max_iter, 0, 255))
 	r, g, b = color, color, color
@@ -146,7 +153,7 @@ func calcRgb(x, y, maxX, maxY int, cs *complexSample) (r, g, b uint8) {
 	return
 }
 
-func iter(r, im float64, maxIter int) int {
+func mandelbrotSet(r, im float64, maxIter int) int {
 	z := complex(0, 0)
 	c := complex(r, im)
 
